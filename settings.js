1e11b7d604b4aa0228471ea51926da57cac0ff3e
Casmendo <isahmusa9921@gmail.com>
2025-02-07 21:26:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'MALAKA-MD~ydIhQBjY#wO4Cpau0u5zyALokELIjyZrDDKsramW-1qE1BHn5TYE' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MALAKA-MD~HQBTlCyI#IAmwEevvBW1PhgFh9o5RskypvyHsweWuKVIHz_DVDxg' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS === undefined ?"true" : process.env.AUTO_REACT_STATUS,
